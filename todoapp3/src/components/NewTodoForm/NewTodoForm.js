1e50e4a6dbd9be35fbaad7702e4b6758951bd5bb
Firat Seven <firat.seven890@gmail.com>
2022-06-30 22:12:04 +0300

    Ornek

---
+++ todoapp3/src/components/NewTodoForm/NewTodoForm.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import "./NewTodoForm.css";
+
+function NewTodoForm({ addTodo, todos }) {
+  const [todo, setTask] = useState({ task: "", id: "", isDone: false });
+
+  const handleInputChange = (e) => {
+    setTask({ task: e.target.value });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (todo.task !== "") {
+      let newTodo = {
+        task: todo.task,
+        id: uuid(),
+        isDone: false,
+      };
+      addTodo([...todos, newTodo]);
+      setTask({ task: "" });
+    } else {
+      alert("Please enter a todo");
+      return false;
+    }
+  };
+  return (
+    <form id="New-Todo-Form" onSubmit={handleFormSubmit}>
+      <label id="new-todo" htmlFor="new">
+        New Todo
+      </label>
+      <div>
+        <input
+          id="new"
+          name="new-todo"
+          type="text"
+          value={todo.task}
+          onChange={handleInputChange}
+        ></input>
+        <button>Add</button>
+      </div>
+    </form>
+  );
+}
+
+export default NewTodoForm;
